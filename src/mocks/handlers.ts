76e28dcfdd9530a5c7e5251094a794f39bf2e607
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-07-01 11:59:25 +0100

    feat: setup msw for api mocking

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,32 @@
 import { http, HttpResponse } from 'msw'
 
 export const handlers = [
-    http.get('https://api.example.com/user', () => {
-        return HttpResponse.json({
-            id: 'abc-123',
-            firstName: 'John',
-            lastName: 'Maverick',
-        })
-    }),
+    // http.get('https://api.example.com/user', () => {
+    //     return HttpResponse.json({
+    //         id: 'abc-123',
+    //         firstName: 'John',
+    //         lastName: 'Maverick',
+    //     })
+    // }),
+
+    http.get('/users', () => {
+        return HttpResponse.json([
+            {
+                id: 'abc-123',
+                firstName: 'John',
+                lastName: 'Maverick',
+            },
+            {
+                id: 'abc-124',
+                firstName: 'Jane',
+                lastName: 'Doe',
+            },
+            {
+                id: 'abc-125',
+                firstName: 'John',
+                lastName: 'Smith',
+            },
+        ]
+        )
+    })
 ]
