77c8040b4936ac6ba6d55beb779efb385f0080c8
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-07-01 13:02:42 +0100

    feat: update app branding and styling setup
    - Change app title from 'Vite + React + TS' to 'Lendsqr'
    - Add Google Fonts (Work Sans) for consistent typography
    - Update main.tsx to use SCSS styling system instead of CSS
    - Prepare for comprehensive styling architecture

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "@styles/main.scss";
 import { RouterProvider } from "react-router";
 import { router } from "./router/routes";
 
