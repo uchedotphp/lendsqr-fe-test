2dc0fe6ac0ff4af18300853f178f81e0e052df79
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-07-01 11:39:19 +0100

    feat: setup msw for api mocking

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import { RouterProvider } from "react-router";
-import { router } from '../router/routes'
-import './index.css'
+import { router } from "../router/routes";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
-);
+async function enableMocking() {
+  // if (process.env.NODE_ENV !== "development") {
+  //   return;
+  // }
+
+  const { worker } = await import("./mocks/browser");
+
+  // `worker.start()` returns a Promise that resolves
+  // once the Service Worker is up and ready to intercept requests.
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  );
+});
