aaab1a2e9a594c271df3085ad2c675fb9df1a9db
nwulu uchechukwu prince <uchedotphp@gmail.com>
2025-07-02 13:34:51 +0100

    fix: Connect MobileMenu button to sidebar toggle via context
    - Use SidebarContext in MobileMenu to allow toggling sidebar from mobile menu button
    - Improves mobile navigation experience

--- src/components/header/MobileMenu.tsx
+++ src/components/header/MobileMenu.tsx
@@ -1,9 +1,13 @@
 import Button from "@components/ui/buttons/Button";
 import MenuIcon from "@assets/icons/menu-icon.svg";
+import SidebarContext from "../../state-management/context/SidebarContext";
+import { useContext } from "react";
 
 const MobileMenu = () => {
+  const sidebar = useContext(SidebarContext);
+
   return (
-    <Button className="btn--flat h-full w-full">
+    <Button className="btn--flat h-full w-full" onClick={sidebar.toggleSidebar}>
       <img src={MenuIcon} alt="menu icon" />
     </Button>
   );
